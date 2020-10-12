f9e65eb407b921adeb47d1a1dd5c63d05658f591
Gram Nitschke <gramnitschke@gmail.com>
2020-10-11 21:29:45 -0700

    player script setup

---
+++ InfiniteForest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GenerateTree generator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generator = GetComponent<GenerateTree>();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
